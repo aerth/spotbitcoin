6981cf590e69d7a7958fa405bca0d052f3550e44
aerth <aerth@riseup.net>
2017-05-24 00:55:48 +0000

    able to change port

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 )
 
 func main() {
 	flag.Parse()
 	addr := "0.0.0.0:80"
+	if a := os.Getenv("ADDR"); a != "" {
+		addr = a
+	}
 	gethttpclient()
 	s := new(System)
 	h := http.DefaultServeMux
 	h.Handle("/current.png", s)
 	h.Handle("/", http.HandlerFunc(home))
-	http.ListenAndServe(addr, h)
+	err := http.ListenAndServe(addr, h)
+	if err != nil {
+
+		println(err.Error())
+	}
+	os.Exit(111)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
